0079d263e00d3dd3cf2c0c62d76026efbb79d556
LautaroGomez10 <113950994+LautaroGomez10@users.noreply.github.com>
2022-12-26 15:15:23 -0300

    lista entrega 3 con uso de eventos, DOM, y json/storage

--- app.js
+++ app.js
@@ -1,69 +1,35 @@
-alert("Bienvenido a Hasbulla Store")
 
-const Hasbullas = [
-    { nombre: "Hasbulla Feliz", precio: 1000 },
-    { nombre: "Hasbulla Enojado", precio: 2000 },
-    { nombre: "Hasbulla Argentino", precio: 10000 },
+
+let Hasbullas = [
+    {
+        id: 1, 
+        nombre: "Hasbulla Feliz", 
+        precio: 1000, 
+    },
 ]
 
-let carro = []
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+let comprar = document.querySelector("#agregar");
 
-let opcion = prompt("Desea adquirir un Hasbulla?") 
-
-while(opcion != "si" && opcion != "no"){
-    alert("porfavor ingrese si o no")
-    opcion = prompt("Desea adquirir un Hasbulla?")
-;
-}
-
-if(opcion == "si"){
-    alert("a continuacion nuestro catalogo")
-    let productos = Hasbullas.map((Hasbullas) => Hasbullas.nombre + " " + Hasbullas.precio + "$");
-    alert(productos.join(", "))
-} else if (opcion == "no"){
-    alert("gracias por venir!!!")
-}
-
-while(opcion != "no"){
-    let productos = prompt("Que Hasbullas deseas?")
-    let precio = 0
-
-    if(productos == "Hasbulla Feliz" || productos == "Hasbulla Enojado" || productos == "Hasbulla Argentino"){
-        switch(productos){
-            case "Hasbulla Feliz":
-                precio = 1000
-                break;
-            case "Hasbulla Enojado":
-                precio = 2000
-                break;
-            case "Hasbulla Argentino":
-                precio = 10000
-                break;
-            default:
-                break;
-        }
-    let unidades = parseInt(prompt("Cuantos quiere llevar?"))  
-    
-    carro.push({productos, unidades, precio})
-    console.log(carro)
-    } else {
-        alert("No tenemos ese Hasbulla")
-    }
-
-    opcion = prompt("desea algun Hasbulla mas?")
-
-    while(opcion === "no"){
-        alert("Gracias por su compra!!!")
-        carro.forEach((carroFinal) => {
-            console.log(`productos: ${carroFinal.productos}, unidades: ${carroFinal.unidades}, precio: ${carroFinal.precio}, total a pagar: ${carroFinal.unidades * carroFinal.precio}`)
+Hasbullas.find((Hasbullas)=>{
+    comprar.addEventListener("click", () =>{
+        carrito.push({
+            id: Hasbullas.id,
+            nombre: Hasbullas.nombre,
+            precio: Hasbullas.precio,
         })
-    break;
-    }
-}
+        console.log(carrito);
+        saveLocal();
+    })
+})
 
-const total = carro.reduce((acc, el) => acc + el.precio * el.unidades, 0)
-console.log(`el monto total por sus Hasbullas es de:  ${total}`)
-alert(`el monto total por sus Hasbullas es de:  ${total}`)
+let carro = document.querySelector("#carro");
 
-alert("Gracias por confiar en nosotros!!")
+carro.addEventListener("click", () =>{
+    const total = carrito.reduce((acc, Hasbullas) => acc + Hasbullas.precio, 0);
+    console.log(`el monto total por sus Hasbullas es de:  ${total}$`)
+})
 
+const saveLocal = () => {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+};
